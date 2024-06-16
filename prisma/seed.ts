5795c810404c48116b9497eadc779f3a182184fa
shuvo-me <shuvo.js.dev@gmail.com>
2024-06-16 17:18:51 +0600

    setup: prisam seeding.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,13 +1,61 @@
 import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 async function main() {
-  console.log("seeding....");
-}
+  const createFakerUsers = () => ({
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    posts: {
+      create: [
+        {
+          title: faker.lorem.sentence(),
+          content: faker.lorem.paragraph(),
+          published: faker.datatype.boolean(),
+        },
+      ],
+    },
+    profile: {
+      create: {
+        bio: faker.person.bio(),
+      },
+    },
+  });
+  const fakeUsers = faker.helpers.multiple(createFakerUsers, { count: 20 });
+  const users = fakeUsers.map((user) =>
+    prisma.user.createMany({ data: { ...user } })
+  );
 
+  const data = await Promise.all([users]);
+  console.log({ data });
+  //   const user = await prisma.user.create({
+  //     data: {
+  //       name: faker.person.fullName(),
+  //       email: faker.internet.email(),
+  //       posts: {
+  //         create: [
+  //           {
+  //             title: faker.lorem.sentence(),
+  //             content: faker.lorem.paragraph(),
+  //             published: faker.datatype.boolean(),
+  //           },
+  //         ],
+  //       },
+  //       profile: {
+  //         create: {
+  //           bio: faker.person.bio(),
+  //         },
+  //       },
+  //     },
+  //   });
+}
 main()
-  .catch((error) => console.error({ error }))
-  .finally(async () => {
+  .then(async () => {
     await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
   });
